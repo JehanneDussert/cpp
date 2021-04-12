08fb39ace893aef60f75b8b0e8801ecccdce47d2
Jehanne DUSSERT <jdussert@student.42.fr>
2021-04-12 22:59:09 +0200

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <iostream>
+#include <ctype.h>
+
+int main	(int argc, char **argv)
+{
+	int i;
+	int j;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    for (j = 1; j < argc ; j++)
+	{
+        for(i = 0; argv[j][i] != '\0'; i++)
+		{
+			argv[j][i] = toupper(argv[j][i]);
+			std::cout << argv[j][i];
+		}
+	}
+	return 0;
+}
