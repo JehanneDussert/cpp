a41d6ef90882e7b4cbf5b2fd378c2994a373dfcb
Jehanne DUSSERT <jdussert@student.42.fr>
2021-04-26 14:44:26 +0200

    ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <iostream>
 #include <ctype.h>
+#include <string.h>
 
 int main	(int argc, char **argv)
 {
 	int i;
 	int j;
+	int k = 1;
+	int n;
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (j = 1; j < argc ; j++)
 	{
-		if (j > 1)
+		for (n = strlen(argv[j]) - 1; n >= 0 && argv[j][n] == ' '; n--)
+			k--;
+		if (k > 1)
 			std::cout << ' ';
-		for(i = 0; argv[j][i] != '\0'; i++)
+		for(i = 0; i <= n; i++){
 			argv[j][i] = toupper(argv[j][i]);
-		std::cout << argv[j];
+			std::cout << argv[j][i];
+		}
+		k++;
 	}
 	std::cout << '\n';
 	return 0;
