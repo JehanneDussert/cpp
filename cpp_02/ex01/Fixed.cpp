dec93a0fe878be6dea0c3658234aed94b10657dd
Jehanne DUSSERT <jdussert@student.42.fr>
2021-05-31 09:24:36 +0200

    fixed

--- cpp_02/ex01/Fixed.cpp
+++ cpp_02/ex01/Fixed.cpp
@@ -10,8 +10,31 @@ Fixed::Fixed(void) : _value(0)
 Fixed::Fixed(Fixed const &src)
 {
 	std::cout << "Copy constructor called\n";
-
 	*this = src;
+
+    return ;
+}
+
+Fixed::Fixed(const int n)
+{
+	std::cout << "Int constructor called\n";
+    this->_value = n << this->_bits;
+
+    //Un constructeur qui prend un const int en paramètre et qui 
+    // le converti à sa valeur fixe(8) correspondante. 
+    // La partie fractionnelle doit être initialisée comme dansl’ex00.
+    return ;
+
+}
+
+Fixed::Fixed(const float n)
+{
+	std::cout << "Float constructor called\n";
+    //•Un constructeur qui prend un const float en paramètre et et 
+    //le convertit à savaleur fixe(8) correspondante. 
+    //La partie fractionnelle doit être initialisée commedans l’ex00.
+    this->_value = roundf(n * (1 <<  this->_bits));
+    std::cout << (this->_value) << std::endl;
     return ;
 }
 
@@ -22,19 +45,19 @@ Fixed::~Fixed(void)
     return ;
 }
 
+int		Fixed::getRawBits(void) const
+{
+	//std::cout << "getRawBits member function called\n";
+
+    return this->_value;
+}
+
 Fixed	&Fixed::operator=(Fixed const &rhs)
 {
 	std::cout << "Assignation operator called\n";
-	this->_value = rhs.getRawBits();
+	this->_value = rhs._value;
 
 	return *this;
-}
-
-int		Fixed::getRawBits(void) const
-{
-	std::cout << "getRawBits member function called\n";
-
-    return this->_value;
 }
 
 void	Fixed::setRawBits(int const raw)
@@ -42,4 +65,37 @@ void	Fixed::setRawBits(int const raw)
 	this->_value = raw;
 
     return ;
+}
+
+float   Fixed::toFloat(void) const
+{
+    float   floatValue;
+
+    floatValue = 0.1; // trouver formule conversion    
+    int intValue;
+
+    intValue = (int)floatValue;
+    std::cout << intValue << std::endl;
+    return floatValue;
+    //•Une fonction membre float toFloat(void) const;
+    //qui convertit un nombres à point fixe en float.
+}
+
+int     Fixed::toInt(void) const
+{
+    int intValue;
+
+    intValue = this->_value / this->_bits; // trouver formule
+    std::cout << "\nhere :";
+    std::cout << intValue << std::endl;
+    return intValue;
+    //•Une fonction membreint toInt(void) const;
+    //qui convertit un nombres à pointfixe en int.
+}
+
+std::ostream    &operator<<(std::ostream &o, Fixed const &rhs)
+{
+    o << rhs.toFloat();
+
+    return o;
 }
--- cpp_02/ex01/Fixed.hpp
+++ cpp_02/ex01/Fixed.hpp
@@ -2,19 +2,31 @@
 # define FIXED_HPP
 
 # include <iostream>
+# include <cmath>
 
 class	Fixed
 {
 	private:
-		int					_value;
-		static int const	_bits = 8;
+        int                 _value;
+		//int					_int;
+        //float               _float;
+		const static int	_bits = 8;
 	public:
 		Fixed(void);
 		Fixed(Fixed const &src);
+		Fixed(const int n);
+        Fixed(const float n);
 		~Fixed(void);
 		int		getRawBits(void) const;
+        float   toFloat(void) const;
+        int     toInt(void) const;
 		void	setRawBits(int const raw);
 		Fixed & operator=(Fixed const &rhs);
 };
+
+std::ostream    &operator<<(std::ostream &o, Fixed const &rhs);
+
+//Vous ajouterez aussi l’overload suivant dans votre header et votre fichier source :
+//•Overload de«qui insère une représentation de votre nombre à point fixe dansl’output demandé.
 
 #endif
