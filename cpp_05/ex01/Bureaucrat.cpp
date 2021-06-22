dc1b3c48a758269abbb530f06700803f55ad0990
Jehanne Dussert <jehanne-dussert@hotmail.fr>
2021-06-22 17:34:25 +0200

    ex01

---
+++ cpp_05/ex01/Bureaucrat.cpp
@@ -0,0 +1,85 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(void)
+{
+	return ;
+}
+
+Bureaucrat::Bureaucrat(std::string name, unsigned int grade) : _name(name), _grade(grade)
+{
+	return ;
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const &src)
+{
+	*this = src;
+
+	return ;
+}
+
+Bureaucrat::~Bureaucrat(void)
+{
+	return ;
+}
+
+Bureaucrat	&Bureaucrat::operator=(Bureaucrat const &rhs)
+{
+	this->_grade = rhs.getGrade();
+
+	return *this;
+}
+
+unsigned int	Bureaucrat::getGrade(void) const
+{
+	return this->_grade;
+}
+
+std::string		Bureaucrat::getName(void) const
+{
+	return this->_name;
+}
+
+void			Bureaucrat::increment(void)
+{
+	if (this->_grade < 2)
+		throw Bureaucrat::GradeTooHighException();
+	this->_grade--;
+}
+
+void			Bureaucrat::decrement(void)
+{
+	if (this->_grade >= 150)
+		throw Bureaucrat::GradeTooLowException();
+	this->_grade++;
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Error. Grade is too high.");
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Error. Grade is too low.");
+}
+
+void			Bureaucrat::signForm(Form& f)
+{
+	try
+	{
+		f.beSigned(*this);
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Form has been signed.\n";
+	}
+
+	return ;
+}
+
+std::ostream    &operator<<(std::ostream &o, Bureaucrat const &rhs)
+{
+	o << rhs.getName() << ", bureaucrat grade " << rhs.getGrade() << ".\n";
+
+	return o;
+}
